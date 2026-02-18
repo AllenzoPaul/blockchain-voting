cb65b8b8cd6fe2e3c8a89d9a281b9c2c32287596
Allenzo Paul <allenzopaul.jr@gmail.com>
2026-02-18 21:26:34 +0530

    Add completed Voting smart contract with lifecycle and voting logic

--- contract/contracts/Voting.sol
+++ contract/contracts/Voting.sol
@@ -5,6 +5,9 @@ contract Voting {
 
     address public admin;
     uint public candidateCount;
+    bool public electionStarted;
+    bool public electionEnded;
+
 
     constructor() {
         admin = msg.sender;
@@ -15,11 +18,22 @@ contract Voting {
         _;
     }
 
+    modifier onlyRegistered() {
+        require(isRegistered[msg.sender], "Not registered");
+        _;
+    }
+
     struct Candidate {
         uint id;
         string name;
         uint voteCount;
     }
+
+    event CandidateAdded(uint id, string name);
+    event VoterRegistered(address voter);
+    event ElectionStarted();
+    event ElectionEnded();
+    event Voted(address voter, uint candidateId);
 
     mapping (uint => Candidate) public candidates;
     mapping (address => bool) public isRegistered;
@@ -33,5 +47,43 @@ contract Voting {
 
         emit CandidateAdded(candidateCount, _name);
     }
-    
-}
+
+    function registerVoter(address _voter) public onlyAdmin {
+        require(!electionStarted, "Election already started");
+        require(!isRegistered[_voter], "Already registered");
+
+
+        isRegistered[_voter] = true;
+
+        emit VoterRegistered(_voter);
+    }
+
+    function startElection() public onlyAdmin {
+        require(!electionStarted, "Election already started");
+        electionStarted = true;
+
+        emit ElectionStarted();
+    }
+
+    function endElection() public onlyAdmin {
+        require(electionStarted, "Election not started");
+        require(!electionEnded, "Election already ended");
+
+        electionEnded = true;
+        
+        emit ElectionEnded();
+    }
+
+    function vote(uint _candidateId) public onlyRegistered {
+        require(electionStarted, "Election not started");
+        require(!electionEnded, "Election already ended");
+        require(!hasVoted[msg.sender], "Already voted");
+        require(_candidateId > 0 && _candidateId <= candidateCount, "Invalid candidate");
+
+        hasVoted[msg.sender] = true;
+        candidates[_candidateId].voteCount++;
+        
+
+        emit Voted(msg.sender, _candidateId);   
+    }
+}   
